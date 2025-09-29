8d34375c8e3095aa4ca49c8aafb1278a9e8d13fa
Sydney March <sydneymarch@Sydneys-MacBook-Pro-2.local>
2025-09-29 09:07:39 -0500

    remvoed dotnet 9 files

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06e0cd574c526a523afbfc8fd8484e0d2e65004b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdca3ba2597896c9fb7719670ae90c8f89fd14e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
